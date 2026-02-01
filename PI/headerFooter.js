edbfbfd4e673e0707e3de8d1d1e9774c8aff5e44
TBG22 <96798083+TBG22tbg@users.noreply.github.com>
2026-02-01 00:10:14 -0300

    Delete PI directory

--- PI/headerFooter.js
+++
@@ -1,3 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navegacao = document.querySelector(".navegacao");
-hamburger.addEventListener("click", () => navegacao.classList.toggle("active"));
